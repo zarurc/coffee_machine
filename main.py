66815125aad2cf8e5e58028f38d01fa7577dccca
Zeke <zarurc@gmail.com>
2023-07-25 16:36:07 -0400

    finished. working machine

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -32,6 +33,9 @@ resources = {
 
 
 Off = False
+water_level = resources["water"]
+milk_level = resources["milk"]
+coffee_level = resources["coffee"]
 income = 0
 
 def print_report():
@@ -42,15 +46,10 @@ def print_report():
 
 def deduct_ingredients(drink_name):
     water_level -= MENU[drink_name]["ingredients"]["water"]
-    milk_level -= MENU[choice]["ingredients"]["milk"]
-    coffee_level -= MENU[choice]["ingredients"]["coffee"]
-    
+    milk_level -= MENU[drink_name]["ingredients"]["milk"]
+    coffee_level -= MENU[drink_name]["ingredients"]["coffee"]
     
 while not Off:
-    water_level = resources["water"]
-    milk_level = resources["milk"]
-    coffee_level = resources["coffee"]
-    
     choice = input("What would you like? (espresso/latte/cappuccino): ")
     if choice == "report":
         print_report()
@@ -59,22 +58,33 @@ while not Off:
     elif choice == "espresso" or choice == "latte" or choice == "cappuccino":
         if water_level < MENU[choice]["ingredients"]["water"]:
             print("Sorry, there is not enough water")
-            Off = True
         elif milk_level < MENU[choice]["ingredients"]["milk"]:
             print("Sorry, there is not enough milk")
-            Off = True
         elif coffee_level < MENU[choice]["ingredients"]["coffee"]:
             print("Sorry, there is not enough coffee")
-            Off = True
         else:
             print("Please insert coins.")
-            quarters_amount = ("How many quarters?")
-            dimes_amount = ("How many dimes?")
-            nickles_amount = ("How many nickles?")
-            pennies_amount = ("How many pennies?")
-            money_added = 0.25 * quarters_amount + 0.1 * dimes_amount + 0.05 * nickles_amount + 0.01 * pennies_amount
+            quarters_amount = int(input("How many quarters?"))
+            dimes_amount = int(input("How many dimes?"))
+            nickles_amount = int(input("How many nickles?"))
+            pennies_amount = int(input("How many pennies?"))
+            money_added = float(0.25 * quarters_amount + 0.1 * dimes_amount + 0.05 * nickles_amount + 0.01 * pennies_amount)
             if money_added < MENU[choice]["cost"]:
                 print("Sorry, that's not enough money. Money refunded")
-                Off = True
             elif money_added == MENU[choice]["cost"]:
-                
+                income += MENU[choice]["cost"]
+                # deduct_ingredients(choice)
+                water_level -= MENU[choice]["ingredients"]["water"]
+                milk_level -= MENU[choice]["ingredients"]["milk"]
+                coffee_level -= MENU[choice]["ingredients"]["coffee"]
+                print(f"Here is your {choice}")
+            else:
+                income += MENU[choice]["cost"]
+                # deduct_ingredients(choice)
+                water_level -= MENU[choice]["ingredients"]["water"]
+                milk_level -= MENU[choice]["ingredients"]["milk"]
+                coffee_level -= MENU[choice]["ingredients"]["coffee"]
+                change = round(money_added - MENU[choice]["cost"], 2)
+                print(f"Here is ${change} in change.")
+                print(f"Here is your {choice}")
+
